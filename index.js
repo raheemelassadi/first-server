434d77fb2c3c7367da932f35036de035c14346f3
raheemelassadi <raheemelassadi1@gmail.com>
2023-01-14 17:26:48 -0800

    added routing and hyperlinks with localhoust routing

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const http = require('http')
-
-const hostname = '127.0.0.1'
-const port = 3000
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
+    console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './'
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break;
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break;
+        case '/contact-me':
+            path += 'contact-me.html'
+            res.statusCode = 200
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404
+            break;
+    }
+    
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.end(data)
+        }
+    })
 })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+
+server.listen(8080, 'localhost', () => {
+    console.log('request made')
 })
+
